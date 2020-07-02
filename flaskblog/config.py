ba80cb198747727046cecb715fb03c49e1c001a1
Aditya Aggarwal <ahunk90@gmail.com>
2020-07-02 22:56:24 +0530

    Configuration Added to restructure

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = 'a6d4c206873961514ec69759a9b6b4f7'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,14 +1,14 @@
 import os, secrets
 from PIL import Image
 from flask_mail import Message 
-from flaskblog import app, mail
-from flask import url_for
+from flaskblog import mail
+from flask import url_for, current_app
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics/' , picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics/' , picture_fn)
 
     output_size =(125,125)
     i = Image.open(picture_path)
