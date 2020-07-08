573bf442cde08eeccec02fc4ceec16a6aff433fe
Aditya Aggarwal <ahunk90@gmail.com>
2020-07-08 22:44:00 +0530

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -7,7 +7,7 @@ database = "dfs23g430okcdn"
 class Config:
     SECRET_KEY = 'a6d4c206873961514ec69759a9b6b4f7'
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
